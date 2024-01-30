4ccef0fb9c58667a9335425789bc7b70a9caf5d7
KY00KIM <hived@naver.com>
2024-01-31 07:25:16 +0900

    Feat healthcheck

--- isee-server/src/app/app.controller.ts
+++ isee-server/src/app/app.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { DefaultDto } from 'src/common/dto/deafult.dto';
+import { ApiTags, ApiOperation, ApiCreatedResponse } from '@nestjs/swagger';
 
 @Controller()
+@ApiTags('iSEE')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('health')
+  @ApiOperation({ summary: '서버 상태 확인.' })
+  @ApiCreatedResponse({ type: DefaultDto<null> })
   getHello(): DefaultDto<null> {
     return this.appService.getHealth();
   }
