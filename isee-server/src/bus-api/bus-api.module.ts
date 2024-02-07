774ce95ac93b6769233282b4c001dab30e83eaff
KY00KIM <hived@naver.com>
2024-02-07 11:30:04 +0900

    Del bus api

--- isee-server/src/bus-api/bus-api.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { BusApiController } from './bus-api.controller';
-import { BusApiService } from './bus-api.service';
-
-@Module({
-  controllers: [BusApiController],
-  providers: [BusApiService],
-})
-export class BusApiModule {}
