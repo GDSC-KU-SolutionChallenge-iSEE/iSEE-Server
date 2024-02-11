a54b9fbc871942cdfca626d4455c1448a43e9d98
KY00KIM <dev.kyoomin@gmail.com>
2024-02-11 15:59:34 +0900

    Fix payload limit

--- isee-server/src/main.ts
+++ isee-server/src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import { json, urlencoded } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.use(json({ limit: '20mb' }));
+  app.use(urlencoded({ extended: true, limit: '20mb' }));
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
